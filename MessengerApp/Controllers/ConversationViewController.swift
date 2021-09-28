cfb2473fef40fcecfa8cff3aa1010f1c8f82c870
Drizzytiger <drake.ewing77@gmail.com>
2021-09-28 14:47:35 -0500

    Fixed issue that was causing a bug in the second video. Ready to go

---
+++ MessengerApp/Controllers/ConversationViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MessengerApp
+//
+//  Created by Drake Ewing on 9/28/21.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool){
+        
+        super.viewDidAppear(animated)
+        
+        let isLoginIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoginIn{
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            
+            present(nac, animated: true)
+        }
+        
+    }
+
+}
+
